2655d9b9b3dadf335daf4dc78dccda396fa772d6
Mario Jason Braganza <jason@janusworx.com>
2024-11-16 14:56:59 +0530

    Figure out how to use godotenv and load env files

--- README.md
+++ README.md
@@ -17,7 +17,7 @@ So rewriting something I use frequently is good motivation
       Nothing set in stone, yet
 - [X] ~~Update Project LICENSE once I figure out above~~ (BSD 2 Clause)
 - [X] Learn the basic go toolchain. does not have to be perfect. Learn industry standards later. Right now I desperately need derb to work on the Pi
-- [ ] Figure out how to read in environment variables from a .env file
+- [X] Figure out how to read in environment variables from a .env file
 - [ ] Write slowly. Learn how types work as you go. 
 - [ ] Blah blah blah  … Lots of missing bits (writing tests, et al)
 - [ ] First run!
--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
 )
 
 // The original Python file was just an imperative set of instructions
@@ -9,5 +12,13 @@ import (
 // what I think are obvious disparate functions
 
 func main() {
-	fmt.Println("Hello World")
+	// First things first. Load up any variables you need from environment files.
+	// Put in a comma seperated list of env files to load
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading environment file(s)")
+	}
+	my_name := os.Getenv("NAME")
+	artist1 := os.Getenv("ARTIST1")
+	fmt.Printf("My name is %s and my favourite modern musician is %s\n", my_name, artist1)
 }
