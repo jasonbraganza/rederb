10e08a5c7ee31fcb498954553cd84393a39a8e84
Mario Jason Braganza <jason@janusworx.com>
2024-11-16 14:05:54 +0530

    Hello World!

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+// The original Python file was just an imperative set of instructions
+// Since I cannot replicate that here, I hope to just build a main function with calls to
+// what I think are obvious disparate functions
+
+func main() {
+	fmt.Println("Hello World")
+}
