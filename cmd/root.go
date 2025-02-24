baf9009ebda232f1cd12a6cedbbf4189a39606f2
Mario Jason Braganza <jason@janusworx.com>
2025-02-24 18:32:13 +0530

    Add version

--- cmd/root.go
+++ cmd/root.go
@@ -23,6 +23,7 @@ and listen to it in your favourite podcast podcast player`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
+	Version: "0.1.0",
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
